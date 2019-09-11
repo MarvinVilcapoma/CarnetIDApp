9a70ad69153b2d3d7b0e7455fc4bb779dafafe2e
kevinbh168 <kevin.brena@tecsup.edu.pe>
2019-09-11 16:24:26 -0500

    Primer Commit

---
+++ app/src/main/java/com/vilcapoma/carnetapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.vilcapoma.carnetapp;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewQrCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+
+            String dni="70774858";
+
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(dni, BarcodeFormat.CODE_128, 200, 160);
+            imageViewQrCode = findViewById(R.id.barcode_image);
+            imageViewQrCode.setImageBitmap(bitmap);
+        } catch(Exception e) {
+            Toast.makeText(this,"Error: "+e.getMessage(),Toast.LENGTH_SHORT);
+        }
+
+    }
+}
